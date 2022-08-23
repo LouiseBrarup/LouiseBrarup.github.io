0e20f51f23531a5536a3e27427619acfb8a33298
LouiseBrarup <lbjorgensen04@gmail.com>
2022-08-23 09:04:14 +0200

    almost

---
+++ portofolie/js/modals.js
@@ -0,0 +1,26 @@
+// VARIABLES
+let ribeModal = document.querySelector('#ribeModal');
+let excaliburModal = document.querySelector('#excaliburModal');
+let alphaModal = document.querySelector('#alphaModal');
+
+// OPEN MODALS
+function ribeModalOpen() {
+    ribeModal.style.display = 'flex';
+}
+function excaliburModalOpen() {
+    excaliburModal.style.display = 'block';
+}
+function alphaModalOpen() {
+    alphaModal.style.display = 'block';
+}
+
+// CLOSE MODALS
+function ribeModalClose() {
+    ribeModal.style.display = 'none';
+}
+function excaliburModalClose() {
+    excaliburModal.style.display = 'none';
+}
+function ribeModalClose() {
+    alphaModal.style.display = 'none';
+}
